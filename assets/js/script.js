db28d689c509d2f9bc7bfa06efd879fdd6cb5f8d
adrianhhd <adrian@globalindointimates.com>
2023-03-07 19:49:34 +0700

    Script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
-const navbarr = document.getElementsByTagName("nav")[0];
-window.addEventListener("scroll", function () {
-  console.log(window.scrollY);
-  if (window.scrollY > 1) {
+const navbarr = document.getElementsByTagName("nav")[0]; // Carikan Elemen pertama (0) nav
+// navbarr menyimpan element html pertama
+// console.log(document.getElementsByTagName("nav")[0])
+
+window.addEventListener("scroll", function () { // saat window di scroll jalankan fungsi
+  if (window.scrollY > 1) { //
     navbarr.classList.replace("bg-transparent", "nav-color");
   } else if (window.scrollY <= 0) {
     navbarr.classList.replace("nav-color", "bg-transparent");
