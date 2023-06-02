c26102acefd9d6979a1196711ed276c70f9222ac
adrianhhd <118419967+adrianhhd@users.noreply.github.com>
2023-06-02 09:28:16 +0700

    Update: scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,25 @@ window.addEventListener("scroll", function () { // saat window di scroll jalanka
     navbarr.classList.replace("nav-color", "bg-transparent");
   }
 });
+
+let sections = document.querySelectorAll('section');
+let navLi = document.querySelectorAll('.linked');
+
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    
+    if (scrollY >= sectionTop ) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+      console.log('harusnya ganti si')
+    }
+  });
+};
